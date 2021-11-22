96b7ff7624f8c0a05d235973f9070ac13e0acad1
kxrt <73589885+kxrt@users.noreply.github.com>
2021-11-22 15:48:22 +0800

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,17 @@
+// breaks at fib(79) and onwards
+
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else {
+        let prev = 0;
+        let final = 1;
+        let store = 0;
+        for (let i = 0; i < n - 1; i = i + 1) {
+            store = prev;
+            prev = final;
+            final = final + store;
+        }
+        return final;
+    }
+}
